8efddfc8548434845d2804b6588341e6fedd6825
AL IMRAN FAKIR <al.imran.fakir.cse@gmail.com>
2021-10-05 14:06:03 +0600

    signupactivity done

--- app/src/main/java/com/example/shahajjer_hat/LoginActivity.java
+++ app/src/main/java/com/example/shahajjer_hat/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.shahajjer_hat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
@@ -12,5 +14,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+
+    }
+
+
+    public void btngo(View view) {
+        Intent intent = new Intent(this,SignUp.class);
+        startActivity(intent);
+
     }
 }
